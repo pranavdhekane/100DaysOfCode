1cf434cf79d68b2a1e5252bd7b1ebc58b2349c63
pranavdhekane <pranavdhekane006@gmail.com>
2024-05-20 22:56:38 +0530

    day 3

--- day3/day3.java
+++ day3/day3.java
@@ -15,6 +15,7 @@ public class day3 {
         return;
     }
     public static void main(String[] args) {
+        @SuppressWarnings("resource")
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
