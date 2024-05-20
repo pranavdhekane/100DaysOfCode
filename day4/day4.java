73e35d44b73c66cd371562f01f4f988168491c23
pranavdhekane <pranavdhekane006@gmail.com>
2024-05-20 22:55:53 +0530

    day4

---
+++ day4/day4.java
@@ -0,0 +1,23 @@
+package day4;
+
+import java.util.Scanner;
+
+public class day4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println("Enter a number to search : ");
+        int n = sc.nextInt();
+
+        for(int i = 0; i < array.length; i++){
+            if(n == array[i]) {
+                System.out.println(n + " found at index " + i);
+                return;
+            }
+        }
+
+        System.out.println("The given number is not present in the array");
+    }
+}
