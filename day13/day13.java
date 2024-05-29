517577b455cbc751840a7ac5afbd76d79d39a4ee
pranavdhekane <pranavdhekane006@gmail.com>
2024-05-29 15:45:03 +0530

    day 13

---
+++ day13/day13.java
@@ -0,0 +1,9 @@
+package day13;
+
+public class day13 {
+    public static void main(String[] args) {
+        String str = "123.23e";
+        Double str1 = Double.valueOf(str);
+        System.out.println(str1);
+    }
+}
